3bb56edbc64aeb278477efd2657dfbf5971d3acd
Ruebenbaer <ansgar.ruetten@gmx.de>
2023-11-26 23:44:56 +0100

    Funktionsliste aufgegeben (scheitert an meinem Unvermögen) Bieter aus erster Zeile einlesen implementiert.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <fstream>
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <vector>
 #include <stdlib.h>
 #include "lvSruct.h"
 
-void einlesen(std::ifstream &file, std::vector<Position> &LV);
-void ErsteZeileEinlesen(std::string &zeile, void (*funktionsListe[256])(std::string, Position&, int),
-						int &anzLeseFunktion, std::vector<Bieter*> &lstBieter);
+void einlesen(std::ifstream &file, std::vector<Position*> &LV);
+void ErsteZeileLesen(std::string, std::vector<Bieter*>&);
+
+void printHelp()
+{
+	std::cout<<"Erforderliche Reihenfolge der Einträge:\t";
+	std::cout<<"PositionNr.\tKurztext\tLangtext\tmitGP\tMenge\tEinheit\tSchwerpunktpos.\tBieter 1\tBieter 2;Bieter 3; usw.\n";
+	std::cout<<"In der ersten Zeile der Datei sind die Namen der Bieter zu hinterlegen\n\n";
+	return;
+}
 
 int main(int argc, char** argv)
 {
@@ -16,6 +24,12 @@ int main(int argc, char** argv)
 		std::cout<<"Verwendung:\n"<<argv[0]<<" [Datei]\n";
 		system("PAUSE");
 		return EXIT_FAILURE;
+	}
+	
+	if(strcmp(argv[1], "--help") == 0)
+	{
+		printHelp();
+		return EXIT_SUCCESS;
 	}
 	
 	std::ifstream file;
@@ -27,7 +41,7 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 	
-	std::vector<Position> LV;
+	std::vector<Position*> LV;
 	
 	std::streampos fileAnfang = file.tellg();
 	
@@ -38,35 +52,26 @@ int main(int argc, char** argv)
 	return EXIT_SUCCESS;
 }
 
-void einlesen(std::ifstream &file, std::vector<Position> &LV)
+void einlesen(std::ifstream &file, std::vector<Position*> &LV)
 {
 	std::streampos fileAnfang = file.tellg();
 	std::string zeile;
 	
-	void (*funktionsListe[256])(std::string, Position&, int);
-	int anzLeseFunktion = 0;
-	std::getline(file, zeile);
-	
 	std::vector<Bieter*> lstBieter;
-	ErsteZeileEinlesen(zeile, funktionsListe, anzLeseFunktion, lstBieter);
+	std::getline(file, zeile);
+	ErsteZeileLesen(zeile, lstBieter);
 
+	std::cout<<"erste Zeile eingelesen\n"<<lstBieter.size()<<" Bieter eingelesen:\n";
+	for(long long unsigned int i = 0; i < lstBieter.size(); i++)
+	{
+		std::cout<<lstBieter[i]->name<<"\n";
+	}
+	
+	Position tempPos;
 	while(!file.eof())
 	{
 		std::getline(file, zeile);
-		
-		size_t pos = 0;
-		size_t neuPos = 0;
-		while(1)
-		{
-			neuPos = zeile.find(";", pos);
-			if(neuPos == std::string::npos)
-			{
-				break;
-			}
-			std::cout<<zeile.substr(pos, neuPos-pos)<<"[semikolon]";
-			pos = neuPos + 1;
-		}
-		std::cout<<"\n";
+		std::cout<<"Verarbeite Zeile: "<<zeile<<"\n";
 	};
 	return;
 }
@@ -80,39 +85,46 @@ void KommaGegenPunkt(std::string str)
 	return;
 }
 
-void Lese_posNr(std::string str, Position &pos, int i)
+void Lese_posNr(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_posNr\n";
 	pos.posNr = str;
 	return;
 }
 
-void Lese_kurzText(std::string str, Position &pos, int i)
+void Lese_kurzText(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_kurzText\n";
 	pos.kurzText = str;
 	return;
 }
 
-void Lese_langText(std::string str, Position &pos, int i)
+void Lese_langText(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_langText\n";
 	pos.langText = str;
 	return;
 }
 
-void Lese_menge(std::string str, Position &pos, int i)
+
+void Lese_menge(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_menge\n";
 	KommaGegenPunkt(str);
 	pos.menge = atof(str.c_str());
 	return;
 }
 
-void Lese_einheit(std::string str, Position &pos, int i)
+void Lese_einheit(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_einheit\n";
 	pos.einheit = str;
 	return;
 }
 
-void Lese_evtlPos(std::string str, Position &pos, int i)
+void Lese_evtlPos(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_evtlPos\n";
 	if(str.empty())
 		pos.evtlPos = false;
 	else
@@ -120,8 +132,9 @@ void Lese_evtlPos(std::string str, Position &pos, int i)
 	return;
 }
 
-void Lese_schwerPunkt(std::string str, Position &pos, int i)
+void Lese_schwerPunkt(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_schwerPunkt\n";
 	if(str.empty())
 		pos.schwerPunkt = false;
 	else
@@ -129,127 +142,35 @@ void Lese_schwerPunkt(std::string str, Position &pos, int i)
 	return;
 }
 
-void Lese_Bieter(std::string str, Position &pos, int i)
+void Lese_Bieter(std::string str, Position &pos, int &i)
 {
+	std::cout<<"Lese_Bieter\n";
 	KommaGegenPunkt(str);
-	pos.lstAngebote[i].EP = atof(str.c_str());
+	pos.lstAngebote[i++].EP = atof(str.c_str());
 	return;
 }
 
-void ErsteZeileEinlesen(std::string &zeile, void (*funktionsListe[256])(std::string, Position&, int),
-						int &anzLeseFunktion, std::vector<Bieter*> &lstBieter)
+void ErsteZeileLesen(std::string zeile, std::vector<Bieter*> &bieter)
 {
-	size_t pos = 0, neuPos = 0;
-	int anzBieter = 0;
-	anzLeseFunktion = 0;
-	std::string begriff;
-	do{
+	size_t pos = 0;
+	size_t neuPos = 0;
+	int stelle = 0;
+	while(1)
+	{
 		neuPos = zeile.find(";", pos);
 		if(neuPos == std::string::npos)
 		{
 			break;
 		}
-		begriff = zeile.substr(pos, neuPos-pos);
-		std::cout<<"Zeile: "<<begriff<<"\n";
-		if("posNr" == begriff)
+		if(stelle > 6)
 		{
-			std::cout<<"posNr gefunden\n";
-			anzLeseFunktion++;
+			std::cout<<"Verarbeite: "<<zeile.substr(pos, neuPos-pos)<<"\n";
+			Bieter* neuBieter = new Bieter;
+			neuBieter->name = zeile.substr(pos, neuPos-pos);
+			bieter.push_back(neuBieter);
 		}
-		if("kurzText" == begriff)
-		{
-			std::cout<<"kurzText gefunden\n";
-			anzLeseFunktion++;
-		}
-		if("langText" == begriff)
-		{
-			std::cout<<"langText gefunden\n";
-			anzLeseFunktion++;
-		}
-		if("mitGP" == begriff)
-		{
-			std::cout<<"mitGP gefunden\n";
-			anzLeseFunktion++;
-		}
-		if("schwerPunkt" == begriff)
-		{
-			std::cout<<"schwerPunkt gefunden\n";
-			anzLeseFunktion++;
-		}
-		if("menge" == begriff)
-		{
-			std::cout<<"menge gefunden\n";
-			anzLeseFunktion++;
-		}
-		if("einheit" == begriff)
-		{
-			std::cout<<"einheit gefunden\n";
-			anzLeseFunktion++;
-		}
-		std::cout<<begriff.compare("Bieter:")<<"\n";
-		if(begriff.compare("Bieter:") < 0)
-		{
-			std::cout<<"Bieter: gefunden\n";
-			/*if(anzBieter > 199)continue;
-			Bieter *bieter = new Bieter;
-			bieter->name = begriff.substr(7, begriff.length() - 7);
-			lstBieter.push_back(bieter);*/
-			anzLeseFunktion++;
-			anzBieter++;
-		}
+		stelle++;
 		pos = neuPos + 1;
-	}while(neuPos != std::string::npos);
-	
-	int i = 0;
-	do{
-		neuPos = zeile.find(";", pos);
-		if(neuPos == std::string::npos)
-		{
-			break;
-		}
-		begriff = zeile.substr(pos, neuPos-pos);
-		if("posNr" == begriff)
-		{
-			funktionsListe[i] = &Lese_posNr;
-			i++;
-		}
-		if("kurzText" == begriff)
-		{
-			funktionsListe[i] = &Lese_kurzText;
-			i++;
-		}
-		if("langText" == begriff)
-		{
-			funktionsListe[i] = &Lese_langText;
-			i++;
-		}
-		if("mitGP" == begriff)
-		{
-			funktionsListe[i] = &Lese_evtlPos;
-			i++;
-		}
-		if("schwerPunkt" == begriff)
-		{
-			funktionsListe[i] = &Lese_schwerPunkt;
-			i++;
-		}
-		if("menge" == begriff)
-		{
-			funktionsListe[i] = &Lese_menge;
-			i++;
-		}
-		if("einheit" == begriff)
-		{
-			funktionsListe[i] = &Lese_einheit;
-			i++;
-		}
-		if(begriff.compare("Bieter:") < 0)
-		{
-			funktionsListe[i] = &Lese_Bieter;
-			i++;
-		}
-		pos = neuPos + 1;
-		if(i > 55)break;
-	}while(neuPos != std::string::npos);
+	}
 	return;
 }
